ba6ffbc4304ca61b41085e8d28093c200d095d53
Devil boy <kws512@hotmail.com>
2012-06-26 13:46:06 -0700

    Added per-mob permissions

--- src/pgDev/bukkit/MCNoTarget/MCNTMainListener.java
+++ src/pgDev/bukkit/MCNoTarget/MCNTMainListener.java
@@ -3,6 +3,7 @@ package pgDev.bukkit.MCNoTarget;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.entity.EntityTargetEvent.TargetReason;
 
 public class MCNTMainListener implements Listener {
 	final MCNoTarget plugin;
@@ -16,8 +17,10 @@ public class MCNTMainListener implements Listener {
 		if (!event.isCancelled()) {
 			if (event.getTarget() instanceof Player) {
 				Player player = (Player) event.getTarget();
-				if (plugin.hasPermissions(player, "mcnotarget.enabled")) {
-					event.setCancelled(true);
+				if (plugin.hasPermissions(player, "mcnotarget." + event.getEntityType().getName().toLowerCase())) {
+					if (event.getReason() == TargetReason.CLOSEST_PLAYER || event.getReason() == TargetReason.RANDOM_TARGET) {
+						event.setCancelled(true);
+					}
 				}
 			}
 		}
